1c4ca16bbbd9d55ecef8018030bb03f79baf6da9
JeeEssEm <greendiesel228@gmail.com>
2023-03-14 12:16:02 +0300

    kaif

--- feedback/forms.py
+++ feedback/forms.py
@@ -10,18 +10,26 @@ class FeedBackForm(django.forms.ModelForm):
         fields = (
             models.FeedBack.email.field.name,
             models.FeedBack.text.field.name,
+            models.FeedBack.attachments.field.name,
         )
         labels = {
             models.FeedBack.email.field.name: 'Email',
             models.FeedBack.text.field.name: 'Текст',
+            models.FeedBack.attachments.field.name: 'Вложения',
         }
 
         help_texts = {
             models.FeedBack.email.field.name: 'Введите ваш email',
             models.FeedBack.text.field.name: 'Введите текст вашей проблемы',
+            models.FeedBack.attachments.field.name: 'Добавьте вложения',
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.visible_fields():
             field.field.widget.attrs['class'] = 'form-control'
+        self.fields[models.FeedBack.attachments.field.name].widget = (
+            django.forms.ClearableFileInput(attrs={
+                'multiple': True
+            })
+        )
--- feedback/models.py
+++ feedback/models.py
@@ -1,5 +1,7 @@
 import django.db.models
 
+import pathlib
+
 
 class FeedBack(django.db.models.Model):
 
@@ -8,6 +10,9 @@ class FeedBack(django.db.models.Model):
         idle = 'idle', 'в процессе'
         answered = 'answered', 'ответ дан'
 
+    def get_upload_folder(instance, filename):
+        return pathlib.Path('upload_to') / str(instance.id) / filename
+
     text = django.db.models.TextField()
     created_on = django.db.models.DateTimeField(auto_now_add=True)
     email = django.db.models.EmailField()
@@ -15,4 +20,16 @@ class FeedBack(django.db.models.Model):
         choices=Statuses.choices,
         default=Statuses.received,
         max_length=16
+    )
+
+
+class Attachment(django.db.models.Model):
+    def get_upload_folder(instance, filename):
+        return pathlib.Path('upload_to') / str(instance.feedback.id) / filename
+
+    file = django.db.models.FileField(upload_to=get_upload_folder, null=True, blank=True)
+    feedback = django.db.models.ForeignKey(
+        to=FeedBack,
+        on_delete=django.db.models.deletion.CASCADE,
+        related_name='attachments'
     )
